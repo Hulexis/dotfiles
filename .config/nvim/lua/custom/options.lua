f4c3280776022d5f44e8b6a48641685f3ef71e14
Arne Johannesen <arne@easee.com>
2022-07-22 07:51:19 +0200

    [Nvim] Change tab to 2 spaces

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -2,12 +2,16 @@
 vim.cmd "au TextYankPost * silent! lua vim.highlight.on_yank()"
 
 -- Set line lenght markers
-vim.opt.colorcolumn = '80,120'
+vim.opt.colorcolumn = "80,120"
 
 -- vim.opt.spell = true
 -- vim.opt.spelllang = 'en'
 
 -- Set yank clipboard to use
-vim.api.nvim_set_option("clipboard","unnamedplus")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
-
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.bo.softtabstop = 2
