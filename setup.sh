a119a196fd5d7ce9f7e8229e24e10c43e9fa1431
Arne Johannesen <arne@easee.com>
2022-10-05 16:56:43 +0200

    Move qtile and alacritty out of .config

--- setup.sh
+++ setup.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
+# Removing stuff
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.zshrc
+
+rm -r ~/.config/alacritty
+rm -r ~/.config/nvim
+rm -r ~/.config/qtile
+
+
+# Setup up neovim 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+ln -s ~/.dotfiles/nvim ~/.config/nvim/lua/custom
 
-mkdir ~/.config/nvim/lua/custom
+# Setup up normal dot files
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
-ln -s ~/.dotfiles/nvim/* ~/.config/nvim/lua/custom
+# Setup alacritty
+ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+
+# Setup qtile
+ln -s ~/.dotfiles/qtile ~/.config/qtile
 
