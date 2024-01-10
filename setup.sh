73838a7361ae3e9495c1f1cbaa8f7a39bd018f7d
Arne Johannesen <arne.johannesen00@gmail.com>
2024-01-10 23:01:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,39 +1,35 @@
 #!/bin/sh
 
-# Removing stuff
-# rm ~/.tmux.conf
-# rm ~/.vimrc
-# rm ~/.zshrc
-#
-# rm -r ~/.config/alacritty
-# rm -r ~/.config/nvim
-# rm -r ~/.config/qtile
-# rm -r ~/.config/rofi
-
-
-# Setup up neovim 
-# Disaple neovim setup
-# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-# ln -s ~/.dotfiles/nvim ~/.config/nvim/lua/custom
-
-ln -s ~/.dotfiles/nvim ~/.config/nvim
+# Setup scripts
+rm -r ~/scripts
+ln -s ~/.dotfiles/scripts ~/scripts
 
 # Setup up normal dot files
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.tmux.conf
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
-# Setup alacritty
+# Setup folders
+rm -r ~/.config/alacritty
+rm -r ~/.config/qtile
+rm -r ~/.config/picom
+rm -r ~/.config/rofi
+rm -r ~/.config/kitty
+rm -r ~/.config/dunst
+rm -r ~/.config/tmux
+rm -r ~/.config/nvim
+
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
-
-# Setup qtile
 ln -s ~/.dotfiles/qtile ~/.config/qtile
-
 ln -s ~/.dotfiles/picom ~/.config/picom
 ln -s ~/.dotfiles/rofi ~/.config/rofi
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/dunst ~/.config/dunst
 ln -s ~/.dotfiles/tmux ~/.config/tmux
+ln -s ~/.dotfiles/nvim ~/.config/nvim
 
-ln -s ~/.dotfiles/scripts ~/scripts
 
