cebf1fe7690a66c519fd12a2923eb9cbdb858295
Arne Johannesen <arne.johannesen00@gmail.com>
2024-01-10 22:52:08 +0100

    Add dunstrc config

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # Removing stuff
-rm ~/.tmux.conf
-rm ~/.vimrc
-rm ~/.zshrc
-
-rm -r ~/.config/alacritty
-rm -r ~/.config/nvim
-rm -r ~/.config/qtile
-rm -r ~/.config/rofi
+# rm ~/.tmux.conf
+# rm ~/.vimrc
+# rm ~/.zshrc
+#
+# rm -r ~/.config/alacritty
+# rm -r ~/.config/nvim
+# rm -r ~/.config/qtile
+# rm -r ~/.config/rofi
 
 
 # Setup up neovim 
 # Disaple neovim setup
 # git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 # ln -s ~/.dotfiles/nvim ~/.config/nvim/lua/custom
+
+ln -s ~/.dotfiles/nvim ~/.config/nvim
 
 # Setup up normal dot files
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
@@ -29,6 +31,9 @@ ln -s ~/.dotfiles/qtile ~/.config/qtile
 
 ln -s ~/.dotfiles/picom ~/.config/picom
 ln -s ~/.dotfiles/rofi ~/.config/rofi
+ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/dunst ~/.config/dunst
+ln -s ~/.dotfiles/tmux ~/.config/tmux
 
 ln -s ~/.dotfiles/scripts ~/scripts
 
