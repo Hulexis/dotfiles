ecf03dbc8e0312cf1ffba275d87f4bd9c76920ab
Arne Johannesen <arne@easee.com>
2024-01-18 12:22:44 +0100

    Add toggle tabs command

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,29 +8,32 @@ local map = vim.keymap.set
 -- local unmap = vim.keymap.del
 
 local function map_common(combo, command, opts)
-  map({ "n", "i", "x", "s" }, combo, command, opts)
+	map({ "n", "i", "x", "s" }, combo, command, opts)
 end
 
 local function map_normal(combo, command, opts)
-  map("n", combo, command, opts)
+	map("n", combo, command, opts)
 end
 
 local function map_visual(combo, command, opts)
-  map("v", combo, command, opts)
+	map("v", combo, command, opts)
 end
 
 --@param s string
 ---@return string
 local function cmd(s)
-  if string.find(s, "<cr>") then
-    return "<cmd>" .. s
-  else
-    return "<cmd>" .. s .. "<cr>"
-  end
+	if string.find(s, "<cr>") then
+		return "<cmd>" .. s
+	else
+		return "<cmd>" .. s .. "<cr>"
+	end
 end
 
 local keymaps = {}
 
 map_normal("<leader>cs", cmd(":noa w"), { desc = "Save file without formatting", remap = true })
+map_normal("<leader>ct", cmd(":set list!"), { desc = "Toggle tabs indicator", remap = true })
+
+map_common("<C-b>", cmd("Neotree toggle"), { desc = "Open filetree", remap = true })
 
 return keymaps
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,11 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python_recommended_style = 0
-vim.o.expandtab = false
+vim.filetype.add({ extension = { mdx = "mdx" } })
+
 vim.shiftwidth = 2
+
+vim.g.python_recommended_style = 0
+
+vim.o.expandtab = false
 vim.o.tabstop = 2
 vim.o.smartident = true
 vim.o.autoindent = true
 
-vim.filetype.add({ extension = { mdx = "mdx" } })
+vim.o.listchars = "tab:!·,trail:·"
+vim.o.list = false
