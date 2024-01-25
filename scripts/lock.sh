0cf7b7b258331714798ba087a953be3ecf897c54
Arne Johannesen <arne.johannesen00@gmail.com>
2024-01-25 19:53:30 +0100

    Pause spotify on lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# betterlockscreen -l & sleep 5 && xset dpms force standby
-
-# dunstctl set-paused true
-# sleep 0.1
-# betterlockscreen -l
-#
-# sleep 5
-# notify-send "Title" "NOW"
+playerctl play-pause --player=spotify
 
 pkill -xu $EUID -USR1 dunst
 i3lock --color 000000 &
