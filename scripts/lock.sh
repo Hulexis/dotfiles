d38138333ffb0745f133f083f9ac350e2d8addff
Arne Johannesen <arne@easee.com>
2024-01-25 17:40:26 +0100

    Update lock command

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-betterlockscreen -l & sleep 5 && xset dpms force standby
+# betterlockscreen -l & sleep 5 && xset dpms force standby
+
+# dunstctl set-paused true
+# sleep 0.1
+# betterlockscreen -l
+#
+# sleep 5
+# notify-send "Title" "NOW"
+
+pkill -xu $EUID -USR1 dunst
+i3lock --color 000000 &
+wait
+pkill -xu $EUID -USR2 dunst
+
+sleep 5 && xset dpms force standby
