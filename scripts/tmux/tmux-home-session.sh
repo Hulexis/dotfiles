a6736448d6003fa8905a463cefa42491f1672304
Arne Johannesen <arne@easee.com>
2024-03-18 14:50:48 +0100

    Add dotfiles to home session

--- scripts/tmux/tmux-home-session.sh
+++ scripts/tmux/tmux-home-session.sh
@@ -6,13 +6,22 @@ cd ~/ || exit
 
 SESSIONEXISTS=$(tmux list-sessions | grep "$session")
 
-if [ "$SESSIONEXISTS" != "" ]
-then
+if [ "$SESSIONEXISTS" != "" ]; then
 	echo "Session $session already running"
-  exit
+	exit
 fi
 
 tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'home'
+
+window=2
+tmux new-window -t $session:$window -n 'dotfiles'
+tmux send-keys -t $session:$window 'cd ~/.dotfiles/' C-m "git pull" C-m "pnpm i" C-m "nvim" C-m
+
+tmux split-window -t $session:$window
+tmux split-window -t $session:$window.1 -h
+tmux split-window -t $session:$window.2
+tmux split-window -t $session:$window.4 -h
+tmux resize-pane -t $session:$window.1 -Z
