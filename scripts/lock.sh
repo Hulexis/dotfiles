d8cfa6cb838b47b093a33bda76d7c160357d66ab
Arne Johannesen <arne.johannesen00@gmail.com>
2024-03-22 02:34:50 +0100

    Fix notifications showing on lock screen

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-playerctl pause --player=spotify
-
-pkill -xu $EUID -USR1 dunst
-i3lock --color 000000 &
-wait
-pkill -xu $EUID -USR2 dunst
-
-sleep 5 && xset dpms force standby
+i3lock --color 000000 & sleep 5 && xset dpms force standby
