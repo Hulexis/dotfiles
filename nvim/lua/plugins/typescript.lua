802cb566bd1fe6f2e5aa6745ac6f171ba240c98d
Arne Johannesen <arne@easee.com>
2024-08-22 18:10:06 +0200

    Make vtsls import typescript types with type

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -1,1 +1,11 @@
-return { import = "lazyvim.plugins.extras.lang.typescript" }
+return {
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{
+		"neovim/nvim-lspconfig",
+		opts = function(_, opts)
+			opts.servers.vtsls.settings.typescript.preferences = {
+				preferTypeOnlyAutoImports = true,
+			}
+		end,
+	},
+}
