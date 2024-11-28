af984872611480f1a94ca1f45c2d812a25b353f9
Arne Johannesen <arne@easee.com>
2024-11-28 14:06:07 +0100

    Fix keychain in fish, also remove fish_variables

--- fish/conf.d/keychain.fish
+++ fish/conf.d/keychain.fish
@@ -1,7 +1,22 @@
-if status is-login
-    and status is-interactive
+# if status is-login
+#     and status is-interactive
+#
+#     for key in $SSH_KEYS_TO_AUTOLOAD
+#         keychain --eval $key | source
+#     end
+# end
+
+if status --is-interactive
+    # keychain --quiet --agents ssh github
 
     for key in $SSH_KEYS_TO_AUTOLOAD
-        set -l temp_output (keychain --quiet --eval $key 2>/dev/null)
+        keychain --quiet --eval $key | source
+    end
+end
+
+begin
+    set -l HOSTNAME (hostname)
+    if test -f ~/.keychain/$HOSTNAME-fish
+        source ~/.keychain/$HOSTNAME-fish
     end
 end
