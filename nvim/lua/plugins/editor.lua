669d79435c89766c5e0d5e5770222f7422b25be6
Arne Johannesen <arne@easee.com>
2025-04-24 15:49:39 +0200

    Don't close neotree after selcting a file

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -84,14 +84,6 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		opts = {
 			close_if_last_window = true,
-			event_handlers = {
-				{
-					event = "file_opened",
-					handler = function()
-						require("neo-tree.command").execute({ action = "close" })
-					end,
-				},
-			},
 			source_selector = {
 				winbar = false,
 				statusline = false,
