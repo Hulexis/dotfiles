622c58cbb56ed768ffd0739cf7b98ddf3e7b24a2
Arne Johannesen <arne@easee.com>
2025-04-25 14:39:20 +0200

    Revert neotree changes

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -84,12 +84,20 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		opts = {
 			close_if_last_window = true,
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
 			source_selector = {
 				winbar = false,
 				statusline = false,
 			},
 			window = {
-				position = "right",
+				-- position = "right",
 				width = 60,
 			},
 		},
