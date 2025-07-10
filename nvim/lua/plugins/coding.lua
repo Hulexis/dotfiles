30c0fbb8b2857d46f63a07ce245a66035ec46afe
Arne Johannesen <arne@easee.com>
2025-07-10 12:17:46 +0200

    chore: nvim meh

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -30,7 +30,7 @@ return {
 		"L3MON4D3/LuaSnip",
 		--   lazy = false,
 		config = function()
-			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
 			require("luasnip").config.setup({
 				update_events = "TextChanged,TextChangedI",
 				enable_autosnippets = true,
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -120,7 +120,7 @@ return {
 					-- 	height = 5,
 					-- 	padding = 1,
 					-- },
-					{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					-- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 					-- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 					-- {
 					-- 	pane = 3,
