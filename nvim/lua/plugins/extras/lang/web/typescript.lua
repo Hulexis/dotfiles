1ccd7531d6ec44b61dbf1f23c544d2e298433053
Arne Johannesen <arne@easee.com>
2025-10-15 19:34:04 +0200

    chore: meh

--- nvim/lua/plugins/extras/lang/web/typescript.lua
+++ nvim/lua/plugins/extras/lang/web/typescript.lua
@@ -8,11 +8,11 @@ return {
 			opts.servers.vtsls.settings.typescript.preferences = {
 				preferTypeOnlyAutoImports = true,
 			}
-			-- Add root_dir configuration
-			opts.servers.vtsls.root_dir = function(fname)
-				return require("lspconfig.util").find_git_ancestor(fname)
-					or require("lspconfig.util").find_node_modules_ancestor(fname)
-			end
+			-- -- Add root_dir configuration
+			-- opts.servers.vtsls.root_dir = function(fname)
+			-- 	return require("lspconfig.util").find_git_ancestor(fname)
+			-- 		or require("lspconfig.util").find_node_modules_ancestor(fname)
+			-- end
 		end,
 	},
 }
