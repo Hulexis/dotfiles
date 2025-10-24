e124054997eed76c4979d5949f1e4de94ab3c50b
Arne Johannesen <arne.johannesen00@gmail.com>
2025-10-24 22:32:56 +0200

    feat: Fix rust auto complete

--- nvim/lua/plugins/extras/coding/blink.lua
+++ nvim/lua/plugins/extras/coding/blink.lua
@@ -1,7 +1,19 @@
 return {
 	"saghen/blink.cmp",
 	opts = function(_, opts)
+		opts.completion.accept = opts.completion.accept or {}
+		opts.completion.accept.auto_brackets = opts.completion.accept.auto_brackets or {}
 		opts.completion.accept.auto_brackets.enabled = false
-		opts.completion.list = { selection = { preselect = false, auto_insert = true } }
+
+		opts.completion.list = {
+			selection = {
+				preselect = false,
+				auto_insert = true,
+			},
+		}
+
+		opts.completion.accept.behavior = "insert"
+
+		opts.completion.list.selection.auto_insert = false
 	end,
 }
