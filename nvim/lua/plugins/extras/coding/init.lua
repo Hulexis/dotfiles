bf2c2bd68a44aaff355a47b662b742e0baef5e8e
Arne Johannesen <arne@easee.com>
2025-11-19 17:40:53 +0100

    feat: add text-case plugin to nvim

--- nvim/lua/plugins/extras/coding/init.lua
+++ nvim/lua/plugins/extras/coding/init.lua
@@ -2,7 +2,8 @@ return {
 	require("plugins.extras.coding.blink"),
 	require("plugins.extras.coding.luasnip"),
 	require("plugins.extras.coding.mini-pairs"),
-	require("plugins.extras.coding.mini-sorround"),
+	require("plugins.extras.coding.mini-surround"),
 	require("plugins.extras.coding.rooter"),
 	require("plugins.extras.coding.ai"),
+	require("plugins.extras.coding.text-case"),
 }
