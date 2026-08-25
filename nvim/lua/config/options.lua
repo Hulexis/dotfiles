84c5e36f6755616ec5cae2c9b736a400079042b8
Arne Johannesen <arne.johannesen00@gmail.com>
2026-08-25 23:42:26 +0200

    meh

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -30,6 +30,9 @@ vim.g.lazyvim_picker = "telescope"
 -- vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.lazyvim_rust_diagnostics = "bacon-ls"
 
+vim.opt.exrc = true
+vim.opt.secure = true
+
 -- local opt = vim.opt
 --
 -- opt.smoothscroll = true
